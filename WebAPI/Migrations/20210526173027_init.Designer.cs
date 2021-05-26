782430b9d62829c4cd797fd5d2771a90fe9a7ea0
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-26 23:01:27 +0530

    aaaaaaaaa

--- WebAPI/Migrations/20210526173027_init.Designer.cs
+++ WebAPI/Migrations/20210526173027_init.Designer.cs
@@ -9,43 +9,43 @@ using WebAPI.Models;
 namespace WebAPI.Migrations
 {
     [DbContext(typeof(DonationDBContext))]
-    [Migration("20210526151324_init")]
+    [Migration("20210526173027_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
+                .HasAnnotation("ProductVersion", "5.0.6")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("WebAPI.Models.DCandidate", b =>
                 {
-                    b.Property<int>("id")
+                    b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("address")
+                    b.Property<string>("Address")
                         .HasColumnType("nvarchar(100)");
 
-                    b.Property<int>("age")
+                    b.Property<int>("Age")
                         .HasColumnType("int");
 
-                    b.Property<string>("bloodGroup")
+                    b.Property<string>("BloodGroup")
                         .HasColumnType("nvarchar(3)");
 
-                    b.Property<string>("email")
+                    b.Property<string>("Email")
                         .HasColumnType("nvarchar(100)");
 
-                    b.Property<string>("fullName")
+                    b.Property<string>("FullName")
                         .HasColumnType("nvarchar(100)");
 
-                    b.Property<string>("mobile")
+                    b.Property<string>("Mobile")
                         .HasColumnType("nvarchar(16)");
 
-                    b.HasKey("id");
+                    b.HasKey("Id");
 
                     b.ToTable("DCandidates");
                 });
