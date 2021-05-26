edff2b0f3dc244211545da3cb298f22f5cf65404
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-26 21:22:06 +0530

    Commit Swagger Config

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using WebAPI.Models;
 
 namespace WebAPI
@@ -25,13 +26,38 @@ namespace WebAPI
             services.AddDbContext<DonationDBContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
 
-
+            services.AddCors(o =>
+            {
+                o.AddPolicy("AllowAll", builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
             services.AddCors();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DonationAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+                {
+                    string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
+                    c.SwaggerEndpoint(
+                        $"{swaggerJsonBasePath}/swagger/v1/swagger.json",                        
+                            "DonationAPI"
+                        );
+                }
+            );
             app.UseCors(options =>
                 options.WithOrigins("http://localhost:3000")
                     .AllowAnyHeader()
